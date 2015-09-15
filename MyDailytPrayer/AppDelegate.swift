5802b3cdeff5c099f3ce40c07e56b84e0bad7401
AApplemacbookairA macbook <applemacbookair@Ashish-MacBook-Air.local>
2015-09-15 20:20:33 +0530

    done

--- MyDailytPrayer/AppDelegate.swift
+++ MyDailytPrayer/AppDelegate.swift
@@ -1,9 +1,9 @@
 //
 //  AppDelegate.swift
-//  MyDailytPrayer
+//  notificationtest
 //
-//  Created by Neal Lester on 9/14/15.
-//  Copyright (c) 2015 Neal Lester. All rights reserved.
+//  Created by A&A macbook on 24/07/15.
+//  Copyright (c) 2015 A&A macbook. All rights reserved.
 //
 
 import UIKit
@@ -12,12 +12,19 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        return true
-    }
+        
+        let notiftype : UIUserNotificationType = UIUserNotificationType.Sound | UIUserNotificationType.Alert
+        
+        let notifsettings:UIUserNotificationSettings = UIUserNotificationSettings(forTypes: notiftype, categories: nil)
+        
+        UIApplication.sharedApplication().registerUserNotificationSettings(notifsettings)
+    
+    
+
+    return true    }
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- MyDailytPrayer/ViewController.swift
+++ MyDailytPrayer/ViewController.swift
@@ -1,9 +1,9 @@
 //
 //  ViewController.swift
-//  MyDailytPrayer
+//  notificationtest
 //
-//  Created by Neal Lester on 9/14/15.
-//  Copyright (c) 2015 Neal Lester. All rights reserved.
+//  Created by A&A macbook on 24/07/15.
+//  Copyright (c) 2015 A&A macbook. All rights reserved.
 //
 
 import UIKit
@@ -19,7 +19,80 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func firebutton(sender: UIButton) {
+    
+       
+        
+    
+        var datecomp:NSDateComponents = NSDateComponents()
+        datecomp.year = 2015;
+        datecomp.month = 09;
+        datecomp.day = 11;
+        datecomp.hour = 07;
+        datecomp.minute = 00;
+        datecomp.timeZone = NSTimeZone.systemTimeZone()
+        
+        var calendar:NSCalendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
+        var date:NSDate = calendar.dateFromComponents(datecomp)!
+      
 
+        
+        var notification:UILocalNotification = UILocalNotification()
+        notification.category = "notibible"
+        notification.alertBody = "notibibletest2"
+        notification.fireDate = date
+        notification.soundName = UILocalNotificationDefaultSoundName
+        notification.repeatInterval = NSCalendarUnit.CalendarUnitDay
+        
+        var datecomp1:NSDateComponents = NSDateComponents()
+        datecomp.year = 2015;
+        datecomp.month = 09;
+        datecomp.day = 11;
+        datecomp.hour = 09;
+        datecomp.minute = 30;
+        datecomp.timeZone = NSTimeZone.systemTimeZone()
+        
+        
+        var calendar1:NSCalendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
+        var date1:NSDate = calendar.dateFromComponents(datecomp1)!
+        
+        
+        var notification1:UILocalNotification = UILocalNotification()
+        notification.category = "notibible"
+        notification.alertBody = "working"
+        notification.fireDate = date
+        notification.soundName = UILocalNotificationDefaultSoundName
+        notification.repeatInterval = NSCalendarUnit.CalendarUnitDay
+        
+        
+        var datecomp2:NSDateComponents = NSDateComponents()
+        datecomp.year = 2015;
+        datecomp.month = 09;
+        datecomp.day = 11;
+        datecomp.hour = 10;
+        datecomp.minute = 00;
+        datecomp.timeZone = NSTimeZone.systemTimeZone()
+        
+        
+        var calendar2:NSCalendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
+        var date2:NSDate = calendar.dateFromComponents(datecomp2)!
+        
+        
+        var notification2:UILocalNotification = UILocalNotification()
+        notification.category = "notibible"
+        notification.alertBody = "10 manikathe"
+        notification.fireDate = date
+        notification.soundName = UILocalNotificationDefaultSoundName
+        notification.repeatInterval = NSCalendarUnit.CalendarUnitDay
+
+            
+            UIApplication.sharedApplication().scheduleLocalNotification(notification)
+        
+    
+    }
+
+
+        
 
 }
 
