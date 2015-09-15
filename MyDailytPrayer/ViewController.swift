3e64e6197db6d45a2a002c1c31836b2a26d14cce
Neal Lester <neal@3dsafety.com>
2015-09-14 18:48:46 -0700

    Initial Commit

---
+++ MyDailytPrayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyDailytPrayer
+//
+//  Created by Neal Lester on 9/14/15.
+//  Copyright (c) 2015 Neal Lester. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
